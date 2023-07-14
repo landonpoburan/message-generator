42c0a5de055137c932e810f2d4c73ffbae03fb2d
Landon Poburan <landon@l2fitness.com>
2023-07-14 14:32:11 -0700

    Mapped out concept

---
+++ script.js
@@ -0,0 +1,10 @@
+// Codecademy JavaScript Random Message Generator
+
+// If today is WEATHER, if you do XYZ, you will FORTUNE
+// ... If Today Is ...
+// Weather Array: Gloomy, Bright & Sunny, Cold or Rainy, Hot & Sweaty
+// ... And You ...
+// Activity Arrary: Netflix & Chill, Walk Along The Beach, Get Lost In A Book, Let Your Freak Flag Fly, Get Lost In The Forest
+// ... Financial Abundance, Exactly What You've Been Seeking, The Person Of Your Dreams, A Tasty Pie
+// ... Might Be In Your Future.
+
